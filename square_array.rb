6f3b94785aa0b346c78224b40b3921dfc43781ef
makafis <vinnyalfieri@gmail.com>
2015-06-04 13:45:54 -0400

    added collect versoin

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,27 @@
+def square_array_with_collect(array)
+  # your code here
+
+
+  holder = array.collect do |x|
+      number = x*x
+      number
+
+  end
+
+  #holder
+
+
+end
+
+
+
 def square_array(array)
   # your code here
 
   holder = []
 array.each do |x|
 
-holder << x*x
+  holder << x*x
 
 end
 
